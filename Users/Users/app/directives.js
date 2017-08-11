2835a57ad7ab376e9ee7274ec3ca24b955fc3e43
vivek4321 <vivekvivek9866@gmail.com>
2017-08-11 14:54:21 -0500

    Moved directives to directives file

---
+++ Users/Users/app/directives.js
@@ -0,0 +1,18 @@
+app.directive('youtube',function(){
+  return{
+    template:'Subscribe to our channel',
+    restrict:'E'
+  }
+});
+app.directive('facebook',function(){
+  return{
+    template:'Like Us',
+    restrict:'E'
+  }
+});
+app.directive('twitter',function(){
+  return{
+    template:'Subscribe to our channel<br>',
+    restrict:'E'
+  }
+});
